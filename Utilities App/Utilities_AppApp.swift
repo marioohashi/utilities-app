e19ee909942f854e08f7ee6bb41f7128c5fd5fb7
Mario Ohashi <mario.ohashi@gmail.com>
2025-10-31 15:16:56 -0300

    Initial Commit

---
+++ Utilities App/Utilities_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Utilities_AppApp.swift
+//  Utilities App
+//
+//  Created by Mario Ohashi on 31/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Utilities_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
